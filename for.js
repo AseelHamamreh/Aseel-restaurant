8f475840d19b037b2916ebeaa43531981fb51c30
Aseel Hamamreh <aseel.hamamreh@hotmail.com>
2021-03-08 15:20:32 +0200

    i added a function feature

--- for.js
+++ for.js
@@ -1,1 +1,37 @@
-var choice = prompt("Are you")
+console.log("if you want to make changes contact me at this number 0324897394");
+
+var Ans = prompt("Are you hungry?");
+
+while (Ans != 'yes' && Ans != 'no') {
+    Ans = prompt("Are you hungry?");
+}
+
+if (Ans == 'yes') {
+    alert("Then you are in the righte place!");
+    var Meal = prompt('How many meals you want to order?');
+    var P = '<img src= "seafood.jpg" />';
+    console.log(P);
+    var Res = '';
+
+    for (var i = 0; i < Meal; i++) {
+        Res = Res + P;
+
+    }
+    console.log(Res);
+    document.write(Res);
+
+
+
+} else if (Ans == 'no') {
+    alert("Then go and do something useful!!");
+}
+
+
+function offers (){
+    document.write("CALL US NOW ");
+    
+} 
+offers();
+offers();
+offers();
+
